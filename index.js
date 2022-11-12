490f735eeec5901700cec7ef5b2ef74c252b108a
alex lieberman <alex.lieberman182@gmail.com>
2022-11-12 13:25:09 -0800

    made info into objects and added table to html

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ fetch('https://www.econdb.com/api/series/CPIUS/?format=json')
         return response.json ()
     })
     .then(function (data) {
-        console.log(data)
+        let dates = Object.values(data.data.dates)
+        let values = Object.values(data.data.values)
+        let table = {}
+        dates.forEach((element, i) => {
+            table[element] = values[i]
+        })
+        // console.log(table)
+        let thead = document.querySelector('#thead')
+        let tbody = document.querySelector('#tbody')
     })
