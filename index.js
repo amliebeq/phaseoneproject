5cbf084400e1ef11ba36c74ccac1ac7f7bc96082
alex lieberman <alex.lieberman182@gmail.com>
2022-11-13 11:35:41 -0800

    add some text, styling and event listener

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ fetch('https://www.econdb.com/api/series/CPIUS/?format=json')
     .then(function (data) {
         let dates = Object.values(data.data.dates)
         let values = Object.values(data.data.values)
-        let table = {}
+        let table = [{}]
         dates.forEach((element, i) => {
             table[element] = values[i]
         })
-        // console.log(table)
-        let thead = document.querySelector('#thead')
-        let tbody = document.querySelector('#tbody')
+        console.log(table)
     })
+const cpiLink = document.getElementById("cpilink")
+const importantLink = document.getElementById("importantlink")
+let openCpiWindow = () => {
+    window.open('https://en.wikipedia.org/wiki/Consumer_price_index', '_blank')  
+}
+cpiLink.addEventListener('click', openCpiWindow)
+let openImportanceWindow = () => {
+    window.open('https://www.bls.gov/cpi/questions-and-answers.htm#:~:text=As%20a%20means%20of%20adjusting,to%20millions%20of%20American%20workers.', '_blank')
+}
+let changeInfoIn = () => {
+    importantLink.innerHTML = 'Click here for some frequently asked questions about CPI'
+}
+importantLink.addEventListener('click', openImportanceWindow)
+// importantLink.addEventListener('mouseover', changeInfoIn)
+
